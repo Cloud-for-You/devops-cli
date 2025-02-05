b863aa2d230be406cfb3109dd8357c154990a9b4
Haton Petr <phaton@vlslinmgmt11.pmb.cz>
2025-02-05 14:25:45 +0100

    Osetreni nepovinneho parametru pro Invite skupiny

--- pkg/gitlab/project.go
+++ pkg/gitlab/project.go
@@ -48,41 +48,44 @@ func CreateProject(client *gitlab.Client, projectName string, namespaceID int, p
 		Visibility:  gitlab.Ptr(gitlab.VisibilityValue(visibility)),
 	}
 
-	maintainerGroup, res, err := CreateGroup(client, *maintainerGroupName, "", "private")
-	if err != nil {
-		if res != nil && res.StatusCode == http.StatusConflict {
-			fmt.Printf("Group '%s' is exists.\n", *maintainerGroupName)
-		} else {
-			log.Fatalf("Failed to create GitLab group '%s': %v\n", *maintainerGroupName, err)
-		}
-	}
-
-	developerGroup, res, err := CreateGroup(client, *developerGroupName, "", "private")
-	if err != nil {
-		if res != nil && res.StatusCode == http.StatusConflict {
-			fmt.Printf("Group '%s' is exists.\n", *developerGroupName)
-		} else {
-			log.Fatalf("Failed to create GitLab group '%s': %v\n", *developerGroupName, err)
-		}
-	}
-
-	fmt.Println(maintainerGroup.Name)
-	fmt.Println(developerGroup.Name)
-
 	project, res, err := client.Projects.CreateProject(projectOptions)
 	if err != nil {
 		log.Fatalf("Failed to create GitLab repository: %v", err)
 	}
 
-	// Invite groups to project
-	err = InviteGroupToProject(client, project.ID, maintainerGroup.ID, gitlab.Ptr(gitlab.MaintainerPermissions))
-	if err != nil {
-		log.Fatalf("Failed to invite maintainer group to project: %v", err)
+	// Create and Invite Groups for members roles
+	// Maintainer
+	if maintainerGroupName != nil && *maintainerGroupName != "" {
+		maintainerGroup, res, err := CreateGroup(client, *maintainerGroupName, "", "private")
+		if err != nil {
+			if res != nil && res.StatusCode == http.StatusConflict {
+				fmt.Printf("Group '%s' is exists.\n", *maintainerGroupName)
+			} else {
+				log.Fatalf("Failed to create GitLab group '%s': %v\n", *maintainerGroupName, err)
+			}
+		}
+
+		err = InviteGroupToProject(client, project.ID, maintainerGroup.ID, gitlab.Ptr(gitlab.MaintainerPermissions))
+		if err != nil {
+			log.Fatalf("Failed to invite maintainer group to project: %v", err)
+		}
 	}
 
-	err = InviteGroupToProject(client, project.ID, developerGroup.ID, gitlab.Ptr(gitlab.DeveloperPermissions))
-	if err != nil {
-		log.Fatalf("Failed to invite developer group to project: %v", err)
+	// Developer
+	if developerGroupName != nil && *developerGroupName != "" {
+		developerGroup, res, err := CreateGroup(client, *developerGroupName, "", "private")
+		if err != nil {
+			if res != nil && res.StatusCode == http.StatusConflict {
+				fmt.Printf("Group '%s' is exists.\n", *developerGroupName)
+			} else {
+				log.Fatalf("Failed to create GitLab group '%s': %v\n", *developerGroupName, err)
+			}
+		}
+
+		err = InviteGroupToProject(client, project.ID, developerGroup.ID, gitlab.Ptr(gitlab.DeveloperPermissions))
+		if err != nil {
+			log.Fatalf("Failed to invite developer group to project: %v", err)
+		}
 	}
 
 	return project, res, nil
